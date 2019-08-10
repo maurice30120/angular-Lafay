208d3994b83e5cd9884399b57e03e7157a75be16
maurice30120 <damien.huyet@gmail.com>
2019-08-10 23:34:53 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoutonMinuteComponent } from './clavier/bouton/bouton-minute/bouton-minute.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoutonMinuteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/clavier/bouton/bouton-minute/bouton-minute.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bouton-minute',
+  templateUrl: './bouton-minute.component.html',
+  styleUrls: ['./bouton-minute.component.css']
+})
+export class BoutonMinuteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
